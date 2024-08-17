a3970dd36e6ba3974f9179d0140bed24f5cc48ae
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 11:02:58 +0100

    second commit with cleaned up files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders heading Simon Walker', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Simon Walker/i);
   expect(linkElement).toBeInTheDocument();
 });
