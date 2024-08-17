094ea2baeae41e7ffeb5d80bacd5f29a8b018869
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:05:45 +0100

    updating app with component styles and tests folders

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders heading Simon Walker', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Simon Walker/i);
-  expect(linkElement).toBeInTheDocument();
-});
