67d8d0970f2cea78fdab4365de84cd161faaf7f7
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:28:13 +0100

    adding basic contact test

---
+++ src/tests/Contact.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Contact from "../components/Contact";
+
+describe("Contact", () => {
+  it("renders contact form", () => {});
+});
