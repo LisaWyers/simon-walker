e4aa004b702646b9392cc38c25d4b1e0e198137a
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:35:23 +0100

    adding basic NavBar test

---
+++ src/tests/NavBar.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import NavBar from "..components/NavBar";
+
+describe("NavBar", () => {
+  it("renders the correct links", () => {});
+});
