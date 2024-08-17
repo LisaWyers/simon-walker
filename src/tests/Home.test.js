01ea166fc96cdc1c7a5fc19d01ae4edd65031998
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:33:54 +0100

    adding home test

---
+++ src/tests/Home.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "..components/Home";
+
+describe("Home", () => {
+  it("renders the correct images", () => {});
+});
