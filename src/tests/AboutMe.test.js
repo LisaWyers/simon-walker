9ab55b272c84be566eab6a47de099634094fca3b
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:26:36 +0100

    adding basic test

---
+++ src/tests/AboutMe.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import AboutMe from "../components/AboutMe";
+
+describe("AboutMe", () => {
+  it("renders the cirrect test and image", () => {});
+});
