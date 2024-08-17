dea23784c5d82ccb7f5709a460a3d3404934c7d5
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-08-17 12:37:01 +0100

    adding testimonials basic test

---
+++ src/tests/Testimonials.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Testimonials from "..components/Testimonials";
+
+describe("Testimonials", () => {
+  it("renders the text", () => {});
+});
